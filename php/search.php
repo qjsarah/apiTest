e59ff56da95b22be6275053256610f16567b7a07
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-07-18 16:42:06 +0800

    search bar

--- php/search.php
+++ php/search.php
@@ -1,11 +1,10 @@
  <nav class="bg-secondary pb-2 rounded-bottom-5 position-fixed top-0 w-100 ">
  <div class="container pt-3 w-100 d-flex justify-content-between align-items-center">
     <h3>MangaVerse</h3>
-      <div class="d-flex float-end">
-        <input type="text" id="title" placeholder="Enter manga title" class="form-control rounded-pill pr-3">
-        <button onclick="fetchManga()" class="btn btn-outline-dark"id="searchBtn">Search</button>
-
-        <div>
+      <div class="d-flex float-end align-items-center">
+        <input type="text" id="title" placeholder="Enter manga title" class="form-control rounded-pill pr-3 my-auto">
+        <button onclick="fetchManga()" class="btn my-auto"id="searchBtn">Search</button>
+      <div>
         <button id="themeToggle" class="btn">
         <span id="themeIcon"><i class="fa-solid fa-sun"></i></span>
       </div>
